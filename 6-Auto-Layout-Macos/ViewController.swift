4b682739ff3af03820aaa09d8576b8631f8fc939
Baris <karalarbaris@gmail.com>
2022-01-03 22:02:30 +0300

    Initial Commit

---
+++ 6-Auto-Layout-Macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  6-Auto-Layout-Macos
+//
+//  Created by Baris Karalar on 3.01.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
